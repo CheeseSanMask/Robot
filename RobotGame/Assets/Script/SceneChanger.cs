87e68f7e576ed642bcecdb2223f0b7f9b5931978
yanagi <aaaaaaaaaaaaaaaaaaaa>
2020-09-29 13:41:25 +0900

    ピンクローター

---
+++ RobotGame/Assets/Script/SceneChanger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+
+    private void Start()
+    {
+        DontDestroyOnLoad( this.gameObject );
+    }
+
+
+    // シーンの遷移
+    public void ChangeScene( string sceneName )
+    {
+        SceneManager.LoadScene( sceneName );
+    }
+}
