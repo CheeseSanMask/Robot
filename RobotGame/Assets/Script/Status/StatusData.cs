0dde5c3ea9d0013bffb22df83434738ea3bcd3ef
yanagi <aaaaaaaaaaaaaaaaaaaa>
2020-10-19 15:55:38 +0900

    ステータスデータ読み込み

---
+++ RobotGame/Assets/Script/Status/StatusData.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[Serializable] public class StatusData : MonoBehaviour
+{
+    // 名前
+    public string[] name_;
+}
+
+
+public class WeaponStatusData : StatusData
+{
+    // 火力
+    public float[] attackPower_;
+
+    // リロード時間
+    public float[] reloadTime_;
+
+    // 総弾数
+    public float[] allBulletCount_;
+
+    // 装填数
+    public float[] ownedBulletCount_;
+}
+
+
+
+public class ArmorStatusData : StatusData
+{
+    // 体力バフ
+    public float[] lifePointBuff_;
+
+    // 防御力
+    public float[] guardPower_;
+
+    // 移動速度デバフ
+    public float[] moveSpeedDebuff_;
+}
+
+
+public class PlayerStatusData : StatusData
+{
+    // 体力
+    public float[] lifePoint_;
+
+    // 移動速度
+    public float[] moveSpeed_;
+
+    // スラスター
+    public float[] thruster_;
+}
--- RobotGame/Assets/Script/Status/StatusLoader.cs
+++ RobotGame/Assets/Script/Status/StatusLoader.cs
@@ -3,90 +3,36 @@ using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-
-[Serializable] public class WeaponData
-{
-    // 火力
-    public float attackPower_;
-
-    // リロード時間
-    public float reloadTime_;
-
-    // 総弾数
-    public float allBulletCount_;
-
-    // 装填数
-    public float ownedBulletCount_;
-}
-
-
-[Serializable] public class ArmorData
-{
-    // 体力バフ
-    public float lifePointBuff_;
-
-    // 防御力
-    public float guardPower_;
-
-    // 移動速度デバフ
-    public float moveSpeedDebuff_;
-}
-
-
-[Serializable] public class PlayerData
-{
-    // 体力
-    public float lifePoint_;
-
-    // 移動速度
-    public float moveSpeed_;
-
-    // スラスター
-    public float thruster_;
-}
-
+using UnityEngine.AddressableAssets;
 
 public class StatusLoader : MonoBehaviour
 {
-    private void Start()
+    // 武器データリスト
+    private WeaponStatusData weaponStatusData_;
+
+    // 装備データリスト
+    private ArmorStatusData armorStatusData_;
+
+    // プレイヤーデータリスト
+    private PlayerStatusData playerStatusData_;
+
+
+    private void LoadData()
     {
-        WeaponData weapon           = new WeaponData();
-        weapon.attackPower_         = 20;
-        weapon.reloadTime_          = 10;
-        weapon.allBulletCount_      = 105;
-        weapon.ownedBulletCount_    = 30;
+        Func<string, string> LoadDataToJson = ( string statusDataName ) =>
+        {
+            string jsonData = null;
 
-        ArmorData armor         = new ArmorData();
-        armor.lifePointBuff_    = 10;
-        armor.guardPower_       = 10;
-        armor.moveSpeedDebuff_  = 5;
+            Addressables.LoadAssetAsync<string>( statusDataName ).Completed += json =>
+            {
+                jsonData = json.Result;
+            };
 
-        PlayerData player   = new PlayerData();
-        player.lifePoint_   = 100;
-        player.moveSpeed_   = 30;
-        player.thruster_    = 100;
+            return jsonData;
+        };
 
-        string weaponJson   = JsonUtility.ToJson( weapon    );
-        string armorJson    = JsonUtility.ToJson( armor     );
-        string playerJson   = JsonUtility.ToJson( player    );
-
-        string path = Application.dataPath+"/EquipmentData/";
-
-        StreamWriter weaponWriter   = new StreamWriter( path+"WeaponData.json",  false );
-        StreamWriter armorWriter    = new StreamWriter( path+"ArmorData.json",   false );
-        StreamWriter playerWriter   = new StreamWriter( path+"PlayerData.json",  false );
-        
-        weaponWriter.WriteLine( weaponJson );
-        armorWriter.WriteLine( armorJson );
-        playerWriter.WriteLine( playerJson );
-
-        weaponWriter.Flush();
-        armorWriter.Flush();
-        playerWriter.Flush();
-
-        weaponWriter.Close();
-        armorWriter.Close();
-        playerWriter.Close();
+        weaponStatusData_   = JsonUtility.FromJson<WeaponStatusData>( LoadDataToJson( "WeaponStatusData" ) );
+        armorStatusData_    = JsonUtility.FromJson<ArmorStatusData>( LoadDataToJson( "ArmorStatusData" ) );
+        playerStatusData_   = JsonUtility.FromJson<PlayerStatusData>( LoadDataToJson( "PlayerStatusData" ) );
     }
 }
